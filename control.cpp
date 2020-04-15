f6ca5e70e54924ff0afc92f7f2836441863ba19c
gevera1 <gevera@rpi.edu>
2020-04-15 15:24:43 -0400

    Control cpp script

--- control.cpp
+++ control.cpp
@@ -1,3 +1,5 @@
+#include <chrono>
+#include <thread>
 #include <wiringPi.h>
 #include <softPwm.h>
 #include <stdio.h>
@@ -14,34 +16,63 @@ void setup( void ) {
 	softPwmCreate(led, 0, 100); 		
 }
 
+void water( int w, int w_f ) {
+	std::chrono::seconds interval( 10 - w_f );
+	system("echo 'Begin Watering...\n'");
+	for (int i = 0; i < 2; i++) {
+		softPwmWrite(pump, 100); delay(w);
+		softPwmWrite(pump, 0); delay(20);
+		std::this_thread::sleep_for( interval );
+	}
+	std::cout << "Done watering.\n\n";
+}
+
+void light( int l, int l_f ) {
+	std::chrono::seconds interval( 10 - l_f);
+	system("echo 'Adjusting LEDs...\n'");
+	for (int j = 0; j < 2; j++) {
+		softPwmWrite(led, 100); delay(l);
+		softPwmWrite(led, 0); delay(20);
+		std::this_thread::sleep_for( interval );
+	}
+	system("echo 'LEDs Adjusted.\n\n'");
+}
+
 int main(int argc, char* argv[]) {
 	setup();
 
 	int id  = std::stoi(argv[1]);
-	int water_int = std::stoi(argv[2]);
-	int light_int = std::stoi(argv[3]);
+	int water_int = std::stoi(argv[2])*100;
+	int light_int = std::stoi(argv[3])*100;
 	int water_freq = std::stoi(argv[4]);
 	int light_freq = std::stoi(argv[5]);
 
 	std::string fname = std::to_string(id) + ".txt";
 	std::string command = "cat " + fname;
+
+	// Output display
+	system("echo 'Making adjustments...\n\n'");
 	system(command.c_str());
-	system("echo 'Watering...\n'");
-	delay(20);
-	for (int i = 0; i < water_freq*5; i++) {
-		softPwmWrite(pump, water_int); delay(950);
-		softPwmWrite(pump, 0); delay(50);
-	}
-	system("echo 'Done watering.\n'");
-	delay(50);
-	system("echo 'Adjusting LEDs...\n'");
-	delay(20);
-	for (int i = 0; i < light_freq*5; i++) {
-		softPwmWrite(led, light_int); delay(450);
-		softPwmWrite(led, 0); delay(50);
-	}
-	std::cout << "LEDs adjusted.\n\n";
 	
+	// Multi-threading
+	std::thread first(water, water_int, water_freq);
+	std::thread second(light, light_int, light_freq);
+
+	// Begin timing
+	auto start = std::chrono::high_resolution_clock::now();	
+
+	// Watering
+	// Adjusting lights
+	
+	// Synchronize threads
+	first.join();
+	second.join();
+
+	// Finish timing
+	auto finish = std::chrono::high_resolution_clock::now();
+	
+	std::chrono::duration<double> elapsed = finish - start;
+	std::cout << "Process duration: " << elapsed.count() << " sec.\n";
 	
 	return 0;
 }
